16fb48cb05fa12bf5b7d4191753e16d3b461b300
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-19 16:51:54 +0900

    rails3-4 complete

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
-
+    @question = Question.new
+    @questions = current_user.group.questions #現在ログインしているユーザーが所属するグループのquestionを全て取得する
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818103047) do
+ActiveRecord::Schema.define(version: 20170819061353) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id", limit: 4
+    t.integer  "user_id",     limit: 4
+    t.text     "text",        limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "key",        limit: 255
     t.text     "detail",     limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.text     "text",       limit: 65535
+    t.integer  "group_id",   limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
