de0021b027a02845be125478edff39b4beb954f2
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-19 17:28:17 +0900

    rails3-5 complete

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,9 @@
 class TopController < ApplicationController
   def index
     @question = Question.new
-    @questions = current_user.group.questions #現在ログインしているユーザーが所属するグループのquestionを全て取得する
+    feed_contents = current_user.group.feed_contents.includes(:content)
+    # 現在ログインしているユーザーのfeed_contentを複数取得する
+    @feed_contents = feed_contents.map(&:content)
+    # feed_contentsからそれぞれcontentのみ取得する
   end
 end
